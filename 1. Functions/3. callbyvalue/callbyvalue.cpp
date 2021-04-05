4edcc3cc3bd730c8baf77bc1b5af372602c3d19a
Keith Tan <24620742+tankeith@users.noreply.github.com>
2021-04-05 11:16:08 +0800

    update return address

--- 1. Functions/3. callbyvalue/callbyvalue.cpp
+++ 1. Functions/3. callbyvalue/callbyvalue.cpp
@@ -1,7 +1,7 @@
 // Shows how when calling by value of variables, an attempt to swap the values of variables don't get passed to main
 // 1. main a: 3, b: 4
-// 2. swap a: 3, b: 4, temp: 3, return adrs: 5
-// 3. swap a: 4, b: 3, temp: 3, return adrs: 5 <-- swap took place only in swap and not transfered to main
+// 2. swap a: 3, b: 4, temp: 3, return adrs: 19
+// 3. swap a: 4, b: 3, temp: 3, return adrs: 19 <-- swap took place only in swap and not transfered to main
 // 4. main a: 3, b: 4
 // Before: a = 3 b = 4
 // After: a = 3 b = 4
--- 1. Functions/4. callbyreference/callbyreference.cpp
+++ 1. Functions/4. callbyreference/callbyreference.cpp
@@ -1,8 +1,8 @@
 // Shows how when calling by reference to the location of variables, an attempt to swap the values of variables works
 // See difference in code in lines 13, 25: int&
 // 1. main a: 3, b: 4
-// 2. swap a: , b: , temp: , return adrs: 5 <-- we evaluate places/links/positions, the local variables pass references to a and b in main
-// 3. swap a: refs main's a = 3, b: refs main's b = 4, temp: , return adrs: 5
+// 2. swap a: , b: , temp: , return adrs: 20 <-- we evaluate places/links/positions, the local variables pass references to a and b in main
+// 3. swap a: refs main's a = 3, b: refs main's b = 4, temp: , return adrs: 20
 // 4. swap a: 4, b: 3, temp: 3 <-- main's a will get value of main's b, and main's b will get value of temp
 // Before: a = 3 b = 4
 // After: a = 4 b = 3
