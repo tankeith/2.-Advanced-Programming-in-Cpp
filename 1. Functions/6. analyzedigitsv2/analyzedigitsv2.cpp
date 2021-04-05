907d2ee3f62227f6f4b1e321e64f1abfe17e0506
Keith Tan <24620742+tankeith@users.noreply.github.com>
2021-04-05 14:01:36 +0800

    complete code changes to analyzedigitsv2

--- 1. Functions/6. analyzedigitsv2/analyzedigitsv2.cpp
+++ 1. Functions/6. analyzedigitsv2/analyzedigitsv2.cpp
@@ -1,16 +1,10 @@
-/* Rewrites printsnumsum using functions. From 1. Introduction to Programming in C++/6. Iterative Statements/3. printsnumsum
-Write a program that reads a positive integer num, prints the number of digits in num and their sum
-Presents the challenge that any function analyzeDigits() can only return 1 value, while 2 are required (countDigits and sumDigits)
-Call by reference can be deployed to resolve this
-Solution 1: int analyzeDigits(int num, int& outSum),
-where num is called by value and outSum is called by reference
-Solution 2: void analyzedDigits(int num, int& outSum, int& outCount)
+/* Solution 2: void analyzedDigits(int num, int& outSum, int& outCount)
 where the function is a void and outSum and outCount are called by reference */
 
 #include <iostream>
 using namespace std;
 
-int analyzeDigits(int num, int& outSum);
+void analyzeDigits(int num, int& outSum, int& outCount);
 
 int main() {
     int num;
@@ -19,14 +13,14 @@ int main() {
     cout << "Please enter a positive integer:\n";
     cin >> num;
 
-    countDigits = analyzeDigits(num, sumDigits);  // assigns return value to countDigits. Because outSum below is called by reference, sumDigits will take whatever value is assigned locally to outSum in analyzeDigits()
+    analyzeDigits(num, sumDigits, countDigits);  // Because outSum and outCount below are called by reference, sumDigits and countDigits will take whatever value is assigned locally to outSum and outCount in analyzeDigits()
 
     cout << num << " has " << countDigits << " digits and their sum is " << sumDigits << endl;
     
     return 0;
 }
 
-int analyzeDigits(int num, int& outSum) {
+void analyzeDigits(int num, int& outSum, int& outCount) {
     int count, sum;
     int currentDigit;
 
@@ -40,5 +34,6 @@ int analyzeDigits(int num, int& outSum) {
         num = num / 10;
     }
     outSum = sum;
-    return count; // because return causes us to jump out of the function this line needs to be placed at the end
+    outCount = count;
+    // void function doesn't return a value
 }
