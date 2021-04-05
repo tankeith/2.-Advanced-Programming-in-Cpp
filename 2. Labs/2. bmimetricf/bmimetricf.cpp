cc8ae1aa765fa6bbbffe865f1fc64efa8fd84818
Keith Tan <24620742+tankeith@users.noreply.github.com>
2021-04-06 00:27:53 +0800

    updated bmimetricf

--- 2. Labs/2. bmimetricf/bmimetricf.cpp
+++ 2. Labs/2. bmimetricf/bmimetricf.cpp
@@ -1,4 +1,4 @@
-// A function to calculate BMI
+// A function to calculate BMI. From 1. Introduction to Programming in C++/3. Labs
 
 // bmimetric: calculates BMI from weight and height in metric
 // Input: weight in kg and height in m
